24feb12e077de198c1a585066cb2b52edccacfdf
Krxer001 <skibidiskeetaa@outlook.com>
2025-05-06 12:53:06 +0200

    Update silly.js

--- silly.js
+++ silly.js
@@ -2,5 +2,7 @@ function selfdestruct() {
     const scriptPath = './config/ChatTriggers/modules/Valley Addons/features/P3StartTimer.js' 
     Client.scheduleTask(1, ()=>FileLib.delete(scriptPath))
 }
-
+register("worldLoad", ()=>{
 selfdestruct()
+})
+
